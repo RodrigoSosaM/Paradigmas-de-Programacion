d05134ce29f8d104ca780fb23cbdcd4883f031a3
RodrigoSosaM <rsosam1900@alumno.ipn.mx>
2023-05-22 00:23:19 -0600

    Oper.py (Video 5)

---
+++ oper.py
@@ -0,0 +1,8 @@
+#=======================================================
+# De functions.py traer las funciones suma y promedio
+#=======================================================
+
+from functions import suma
+print(suma(100 , 200))
+print(promedio(100 , 200)
+
