1f315ea2410b97c7eee5b1a45bbde85620a4578b
RodrigoSosaM <rsosa1900@alumno.ipn.mx>
2023-06-23 02:34:47 -0600

    Video 11 hasta minuto 3:56

---
+++ MPI (V9 y V10)/05_asincrono.py
@@ -0,0 +1,77 @@
+import multiprocessing as mp
+import numpy as np
+import time
+
+def my_function(i , param1 , param2 , param3):
+
+    #========================
+    # Calcula un polinomio
+    #========================
+
+    result = param1**2 + param2 + param3
+
+    #============================
+    # Se hace tonta 2 segundos
+    #============================
+
+    time.sleep(2)
+    return (i , result)
+
+def get_result(result):
+    
+    #==================================
+    # Se inscribe en la lista global
+    # (mal estilo de programación
+    #==================================
+
+    global results
+    results.append(result)
+
+#======================
+# Programa Principal
+#======================
+
+if __name__ == "__main__":
+
+    #==================================
+    # Matriz de 10x3 números al azar
+    #==================================
+
+    params = np.random.random((10, 3))*100.0
+    results = []
+    ts = time.time()
+
+    #===============================
+    # Un grupo de procesos (pool)
+    #===============================
+
+    pool = mp.Pool(mp.cpu_count())
+
+    #=========================================
+    # Loop de primera dimensión del arreglo
+    #=========================================
+    
+    for i in range(0 , params.shape[0]):
+
+        #=======================================
+        # Correr asincrónicamente my_function 
+        # con argumentos args y cuando
+        # termine correr get_result
+        #=======================================
+
+        pool.apply_async(my_function , args = (i , params[i , 0] , params[i , l] params[i , 2]) , callback = get_result)
+
+        #===================
+        # Cerrar el grupo
+        #===================
+
+        pool.close()
+
+        #================================
+        # Esperar que termine el grupo
+        #================================
+
+        pool.join()
+
+        print("Tiempo en paralelo = " , time.time()-ts)
+        print(results)
